afc951e7bd06608177913b612a0a3287170ac7c6
thomas.mmcr <thomas.mmcr2022@gmail.com>
2025-03-24 12:58:31 +0000

    Added frontend form validation for add item page

--- src/routers/page_router.py
+++ src/routers/page_router.py
@@ -21,6 +21,13 @@ async def browse(request: Request, user: User = Depends(get_current_user_or_redi
         request=request, name="pages/browse.html", context={"path": path, "user": user.get_user_public() if user else None}
     )
 
+@router.get("/add", tags=["Pages"], response_class=HTMLResponse)
+async def add(request: Request, user = Depends(get_current_user_or_redirect)):
+    path = request.url.path
+    return templates.TemplateResponse(
+        request=request, name="pages/add.html", context={"path": path, "user": user.get_user_public() if user else None}
+    )
+
 @router.get("/unauthorised", tags=["Pages"], response_class=HTMLResponse)
 async def unauthorised(request: Request, old_path: str = Query("old_path"), user: User = Depends(get_current_user)):
     return templates.TemplateResponse(
